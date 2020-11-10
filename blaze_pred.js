398671866b29f27c80585b000b639f8b78aa6b0d
Divyam10 <divyamsheth4@gmail.com>
2020-11-10 20:46:03 +0530

    .video attributes

--- blaze_pred.js
+++ blaze_pred.js
@@ -16,12 +16,12 @@
     }
     );
     video.addEventListener('play',function()
-                          {
-        draw(this, context,640,480);
+                          {                 
+        draw(this, context,680,480);
     },false);
     
     async function draw(video,context, width, height)
-    {
+    {   
         context.drawImage(video,0,0,width,height);
         const model = await blazeface.load();
         const returnTensors = false;
@@ -50,7 +50,7 @@
                     bbox_width = bbox_2 - bbox_0 - x;
                     bbox_height = bbox_3 - y;
             }
-                else if ((bbox_2 + bbox_delta) >= 640){
+                else if ((bbox_2 + bbox_delta) >= width){
                 // crop = face[bbox[1]:bbox[3], bbox[0] - (face.shape[1] - bbox[2]):(face.shape[1] - 1), :]}
                     var x =  bbox_0 + (height - bbox_2);
                     var y = bbox_1;
@@ -128,6 +128,139 @@
 
             } 
            }
-        setTimeout(draw,250,video,context,width,height);
+        setTimeout(draw,1000/3,video,context,video.videoWidth,video.videoHeight);
+    }
+})();
+(function() {
+    var canvas = document.getElementById('canvas'),
+        context = canvas.getContext('2d'),
+        video = document.getElementById('webcam');
+    
+    navigator.getMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+    
+    navigator.getMedia({
+        video:true,
+        audio:false
+    }, function(stream){
+        video.srcObject = stream;
+        video.play();
+    }, function(error){
+        //error.code
+    }
+    );
+    video.addEventListener('play',function()
+                          {                 
+        draw(this, context,680,480);
+    },false);
+    
+    async function draw(video,context, width, height)
+    {   
+        context.drawImage(video,0,0,width,height);
+        const model = await blazeface.load();
+        const returnTensors = false;
+        const predictions = await model.estimateFaces(video, returnTensors);
+          if (predictions.length > 0)
+          {
+           for (let i = 0; i < predictions.length; i++) {
+           const start = predictions[i].topLeft;
+           const end = predictions[i].bottomRight;
+           var probability = predictions[i].probability;
+           const size = [end[0] - start[0], end[1] - start[1]];
+           const bbox_0 = start[0];
+           const bbox_1 = start[1];
+           const bbox_2 = end[0];
+           const bbox_3 = end[1];
+
+           var bbox_width = end[0] - start[0];
+           var bbox_height  = end[1] - start[1];
+           const bbox_delta = (bbox_height - bbox_width) / 2;
+            if (bbox_height > bbox_width){
+
+                if ((bbox_0 - bbox_delta) < 0){
+                // crop = face[bbox[1]:bbox[3], 0:bbox[2] + bbox[0], :]};
+                    var x = 0;
+                    var y = bbox_1;
+                    bbox_width = bbox_2 - bbox_0 - x;
+                    bbox_height = bbox_3 - y;
+            }
+                else if ((bbox_2 + bbox_delta) >= width){
+                // crop = face[bbox[1]:bbox[3], bbox[0] - (face.shape[1] - bbox[2]):(face.shape[1] - 1), :]}
+                    var x =  bbox_0 + (height - bbox_2);
+                    var y = bbox_1;
+                    bbox_width = height-1 - x ;
+                    bbox_height = bbox_3 - y;
+            }
+                else{
+                // crop = face[bbox[1]:bbox[3], bbox[0] - bbox_delta:bbox[2] + bbox_delta, :]}}
+                    var x = bbox_0  +  bbox_delta;
+                    var y = height - 1;
+                    bbox_width = bbox_2 -  bbox_delta -  x; 
+                    bbox_height = bbox_3 - y;
+            }}else if(bbox_height < bbox_width){
+                if ((bbox_1-bbox_delta) < 0){
+                    // crop = face[0:bbox[3]+bbox[1], bbox[0]:bbox[2], :]}
+                    var x = bbox_0; 
+                    var y = 0;
+                    bbox_width = bbox_2 - x; 
+                    bbox_height = bbox_3+bbox_1 - y;
+                }else if (((bbox_3+bbox_delta) >= width)){
+                // crop = face[bbox[1]-(bbox[3]-face.shape[0]):(face.shape[0]-1), bbox[0]:bbox[2], :]}
+                    var x = bbox_0;
+                    var y = bbox_1-(bbox_3-width);
+                    bbox_width = bbox_2 - x; 
+                    bbox_height = (width-1) - y;
+                }else{
+                // crop = face[bbox[1]-bbox_delta:bbox[3]+bbox_delta, bbox[0]:bbox[2], :]}
+                    var x = bbox_0;
+                    var y = bbox_1+bbox_delta;
+                    bbox_width = bbox_2 - x;
+                    bbox_height = bbox_3-bbox_delta - y;
+            }}else{
+            // crop = face[bbox[1]:bbox[3], bbox[0]:bbox[2], :]}}
+                var x = bbox_0; 
+                var y = bbox_1;
+                bbox_width = bbox_2 - x; 
+                bbox_height = bbox_3 - y;
+            }
+            console.log((bbox_width*bbox_height)/(width*height))
+           // Render a rectangle over each detected face.
+           if ((bbox_width*bbox_height)/(width*height)>=0.2 & (bbox_width*bbox_height)/(height*width)<= 0.65){
+           
+           const landmarks  = predictions[i].landmarks
+           if (landmarks[0][0]<landmarks[2][0] & landmarks[1][0]>landmarks[2][0] ){
+           context.beginPath();
+           context.strokeStyle="green";
+           context.lineWidth = "4";
+        //    context.rect(start[0], start[1],size[0], size[1]);
+           context.rect(x,y,bbox_width,bbox_height)
+           context.stroke();
+
+            for(let lands = 0; lands < landmarks.length; lands++){
+                    context.beginPath();
+                    context.strokeStyle="red";
+                    context.arc(landmarks[lands][0],landmarks[lands][1],2,0,2*Math.PI)
+                    context.stroke();
+            }
+           
+           var prob = (probability[0]*100).toPrecision(5).toString();
+           var text = prob+"%";
+           context.fillStyle = "red";
+           context.font = "13pt sans-serif";
+           context.fillText(text,x+5,y+20);
+           }
+        else {
+            alert('Look Straight')
+        }}
+            else if ((bbox_width*bbox_height)/(width*height)<0.2) {
+                alert('Come Close')
+            }
+            else {
+                alert('Go behind')
+            }
+
+
+            } 
+           }
+        setTimeout(draw,1000/3,video,context,video.videoWidth,video.videoHeight);
     }
 })();
